d3f5d9a2266b32441acc1687d38ae514dc0dcea5
lvwenbo <443818654@qq.com>
2017-06-16 14:35:18 +0800

    添加项目文件。

---
+++ DBLib/Utils/MyHiberate.cs
@@ -0,0 +1,51 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLib.Utils
+{
+    public class MyHiberate
+    {
+        private static readonly ISessionFactory sessionFactory;
+
+        private static string HibernateHbmXmlFileName = "hibernate.cfg.xml";
+
+        //private static ISession session
+
+        static MyHiberate()
+
+        {
+
+            sessionFactory = new Configuration().Configure().BuildSessionFactory();
+
+        }
+
+        public static ISessionFactory getSessionFactory()
+
+        {
+
+            return sessionFactory;
+
+        }
+
+        public static ISession getSession()
+
+        {
+
+            return sessionFactory.OpenSession();
+
+        }
+
+        public static void closeSessionFactory()
+
+        {
+
+
+
+        }
+    }
+}
---
+++ NhibernateDemo/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using DBLib;
+using DBLib.Model;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NhibernateDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// 简单的NHibernate实例 运行app实现插入一条数据
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            My();
+        }
+
+        private void My()
+        {
+            TestSaveUser();
+        }
+        public void TestSaveUser()
+
+        {
+
+            try
+            {
+                User user = new User();
+                //"james","123",'m',20,"00544"
+                user.Password = "123";
+                user.Username = "Tommy";
+                user.Phone = "110";
+                user.Gender = 'm';
+                user.Age = 1;
+
+
+                ISession session = DBLib.Utils.MyHiberate.getSession();
+
+                ITransaction tx = session.BeginTransaction();
+
+                session.Save(user);
+
+                tx.Commit();
+
+                session.Close();
+                MessageBox.Show("Insert Success:"+user.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
